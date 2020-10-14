43cfba84f014079bc15ae506bbecf943199fe571
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-10-14 12:59:17 +0530

    Initial commit

---
+++ src/brickBracker/Main.java
@@ -0,0 +1,14 @@
+package brickBracker;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame f=new JFrame();
+        f.setBounds(10,10,800,700);
+        f.setTitle("BRICK THE BALL");
+        f.setResizable(false);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
