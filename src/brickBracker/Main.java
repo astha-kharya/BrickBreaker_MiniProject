06ed0601895c26882195083b3a4eb565284ad923
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-10-16 12:11:56 +0530

    make panel,frame and add ball,paddle

--- src/brickBracker/Main.java
+++ src/brickBracker/Main.java
@@ -1,14 +1,18 @@
 package brickBracker;
 
-import javax.swing.*;
+import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame f=new JFrame();
-        f.setBounds(10,10,800,700);
-        f.setTitle("BRICK THE BALL");
-        f.setResizable(false);
-        f.setVisible(true);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame frame=new JFrame();
+        frame.setSize(800,700);
+        frame.setTitle("THE BREAKOUT GAME");
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        Panel panel=new Panel();
+        frame.add(panel);
+
     }
 }
